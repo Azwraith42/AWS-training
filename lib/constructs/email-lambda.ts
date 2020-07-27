da9544b540422524bf3307a1705d312da3b33401
Alec Reeves <areeves@cj.com>
2020-07-27 15:01:52 -0700

    test for environment variable

--- lib/constructs/email-lambda.ts
+++ lib/constructs/email-lambda.ts
@@ -1,23 +1,23 @@
 import * as cdk from '@aws-cdk/core';
 import * as l from '@aws-cdk/aws-lambda';
-import * as sns from '@aws-cdk/aws-sns';
+import {ITopic, Topic} from '@aws-cdk/aws-sns';
 import * as sub from "@aws-cdk/aws-sns-subscriptions";
 
 export interface EmailLambdaProps {
-    topic?: sns.Topic,
+    topic?: ITopic,
     email: string
 }
 
 export class EmailLambda {
 
-    public readonly topic: sns.Topic;
+    public readonly topic: ITopic;
     public readonly email: string;
     public readonly lambda: l.Function;
 
     constructor(scope: cdk.Construct, id: string, props: EmailLambdaProps) {
 
         this.email = props.email;
-        this.topic = props.topic || new sns.Topic(scope, 'EmailLambdaTopic');
+        this.topic = props.topic || new Topic(scope, 'EmailLambdaTopic');
 
         this.lambda = new l.Function(scope, 'LambdaFunction', {
             runtime: l.Runtime.NODEJS_12_X,
@@ -28,7 +28,7 @@ export class EmailLambda {
             }
         });
 
-        this.topic.addSubscription(new sub.EmailSubscription(props.email))
+        this.topic.addSubscription(new sub.EmailSubscription(this.email))
         this.topic.grantPublish(this.lambda)
     }
 }
--- test/aws-training.test.ts
+++ test/aws-training.test.ts
@@ -1,15 +1,17 @@
-import {
-    expect as expectCDK,
-    haveResource,
-    HaveResourceAssertion,
-    haveResourceLike,
-    ResourcePart
-} from '@aws-cdk/assert';
+import {expect as expectCDK, haveResource, haveResourceLike, ResourcePart, SynthUtils} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as sns from '@aws-cdk/aws-sns';
 import {Runtime} from "@aws-cdk/aws-lambda";
 import {AwsTrainingStack} from '../lib/aws-training-stack';
-import {EmailLambda} from "../lib/constructs/email-lambda";
+import {EmailLambda, EmailLambdaProps} from "../lib/constructs/email-lambda";
+import {synthesize} from "@aws-cdk/core/lib/private/synthesis";
+import {ConstructNode, Fn, Reference, Resource, Stack} from "@aws-cdk/core";
+import fn = jest.fn;
+import {LogicalIDs} from "@aws-cdk/core/lib/private/logical-id";
+import * as sub from "@aws-cdk/aws-sns-subscriptions";
+import {PolicyStatement, AddToResourcePolicyResult, IGrantable, Grant} from '@aws-cdk/aws-iam';
+import {CfnTopic, ITopic, ITopicSubscription, Topic} from "@aws-cdk/aws-sns";
+import {IResource} from "@aws-cdk/core/lib/resource";
+import { Metric } from '@aws-cdk/aws-cloudwatch';
 
 describe('AWS Training Stack', () => {
 
@@ -37,16 +39,33 @@ describe('AWS Training Stack', () => {
             email
         });
 
-        expectCDK(stack).to(haveResource("AWS::SNS::Subscription",{
+        expectCDK(stack).to(haveResource("AWS::SNS::Subscription", {
             Protocol: "email",
             Endpoint: email
         }));
     });
 
+    test('Function has topicArn as environment variable', () => {
+        const stack = new cdk.Stack();
+        const email = "someEmail";
+        const uniqueId = 'SomeId';
+        const topic = new Topic(stack, uniqueId);
 
+        new EmailLambda(stack, 'testEmailLambda', {
+            email,
+            topic
+        });
+
+        expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
+            Environment: {
+                Variables: {
+                    TOPIC_ARN: {
+                        Ref: stack.getLogicalId(topic.node.defaultChild as CfnTopic)
+                    }
+                }
+            }
+        }));
+
+    });
 });
-
-
-
-
 
